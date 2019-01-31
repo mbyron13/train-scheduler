3b59eda540938f73504f449cd337cbcdf265f6c0
Mike B <michael.byron@gmail.com>
2019-01-31 14:35:54 -0500

    created front end

---
+++ assets/javascript/js.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyAVPWQ_QKSudxpXmVTc5UtSyTagXGb6fAM",
+    authDomain: "train-activity-527f0.firebaseapp.com",
+    databaseURL: "https://train-activity-527f0.firebaseio.com",
+    projectId: "train-activity-527f0",
+    storageBucket: "train-activity-527f0.appspot.com",
+    messagingSenderId: "955581655122"
+  };
+  firebase.initializeApp(config);
